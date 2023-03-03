9e67d2374837552d342cc446d5e0bd263b292b7b
Merry Rosalie <z5391208@ad.unsw.edu.au>
2023-03-03 23:01:10 +1100

    feat: added navbar

---
+++ src/pages/plan.tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from "next";
+import Layout from "~/components/Layout";
+import Navbar from "~/components/Navbar";
+
+const Plan: NextPage = () => {
+  return (
+    <Layout>
+      <></>
+    </Layout>
+  );
+};
+
+export default Plan;
